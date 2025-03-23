1e88a0f1685e8d40aa9b440ca849cf07e8a47976
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-23 16:02:45 +0300

    dom elements selcted

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select all necessary DOM elements
+const poster = document.getElementById('poster');
+const title = document.getElementById('title');
+const runtime = document.getElementById('runtime');
+const showtime = document.getElementById('showtime');
+const tickets = document.getElementById('tickets');
+
+
